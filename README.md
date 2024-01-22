e7fe691813ed79de88b48ff44847d1826843fba0
Avik Panda <98453311+avikpanda96@users.noreply.github.com>
2024-01-22 13:44:34 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,3 @@
+# MAITYSTORE
+E commerce website build with Frontend using React, Backend using Strapi, and for Payment using Stripe.
+![Maitystore-Google-Chrome-2023-02 (4)](https://user-images.githubusercontent.com/107167604/220445980-86db657d-c880-4e26-99fb-3f7a6e54038f.gif)
---
+++ client/src/App.js
@@ -0,0 +1,62 @@
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Outlet
+} from "react-router-dom";
+import Navbar from "./components/Navbar/Navbar";
+import Footer from "./components/Footer/Footer";
+import Home from "./pages/Home/Home";
+import Product from "./pages/Product/Product";
+import Products from "./pages/Products/Products";
+import "./app.scss"
+import Contact from "./components/Contact/Contact";
+// import FeaturedProducts from "./components/FeaturedProducts/FeaturedProducts";
+
+const Layout = () => {
+  return (
+    <div className ="app">
+      <Navbar/>
+      <Outlet/>
+      <Footer/>
+    </div>
+  )
+}
+
+const router = createBrowserRouter([
+  {
+    path:"/",
+    element:<Layout />,
+    children: [
+      {
+        path:"/",
+        element:<Home/>,
+      },
+      {
+        path:"/products/:id",
+        element:<Products/>,
+      },
+      {
+        path:"/product/:id",
+        element:<Product/>,
+      },
+      {
+        path:"/Contact/",
+        element:<Contact/>,
+      },
+      // {
+      //   path:"/Store/",
+      //   element:<FeaturedProducts/>,
+      // }
+    ],
+  },
+ 
+]);
+function App() {
+  return (
+    <div>
+      <RouterProvider router={router} />
+    </div>
+  );
+}
+
+export default App;
